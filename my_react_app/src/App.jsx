44a606ae43c9b57a19707f09b6d9b558d28ceba8
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-11-16 12:08:02 -0500

    Calling the Rating component in App with rating attribute

--- my_react_app/src/App.jsx
+++ my_react_app/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Products from "./Components/Products";
 import Button from "react-bootstrap/Button"
+import Rating from "./Components/Rating";
 
 function formatName(user){
   return user.firstName + ' ' + user.lastName;
@@ -23,6 +24,11 @@ function App() {
       <img src={user.imageURL} />
       <Products />
       <Button variant="primary" disabled={!isValid}>Default</Button>
+      <Rating rating="1" />
+      <Rating rating="2" />
+      <Rating rating="3" />
+      <Rating rating="4" />
+      <Rating rating="5" />
     </div>
   );
 }

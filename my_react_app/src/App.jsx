5db5a396f4e1c4cb914d5e9c99eec8104fd8619c
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-11-16 11:03:16 -0500

    Importing and using Products component in App

--- my_react_app/src/App.jsx
+++ my_react_app/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Products from "./Components/Products";
 
 function App() {
   return (
     <div>
       <h1>Hello, Fabian Lopez</h1>
+      <Products />
     </div>
   );
 }

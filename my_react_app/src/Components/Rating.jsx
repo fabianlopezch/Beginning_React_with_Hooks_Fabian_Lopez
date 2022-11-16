3b686572e159d3298dbb791578bf01718814828b
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-11-16 12:05:08 -0500

    Adding the Rating component (with a props argument)

---
+++ my_react_app/src/Components/Rating.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Rating(props) {
+    return (
+        <div>
+            <h2>Rating: {props.rating}</h2>
+        </div>
+    );
+}
+
+export default Rating;

4fd1f46f43f4e1d7427b07876156baef8396d4ce
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-11-16 18:16:12 -0500

    Creating a layout containing left-aligned media objects

--- my_react_app/src/Components/Product.jsx
+++ my_react_app/src/Components/Product.jsx
@@ -3,8 +3,24 @@ import Rating from "./Rating";
 
 const Product = (props) => {
     return (
-        <div>
-            
+        <div className="d-flex">
+            <div className="flex-shrink-0">
+                <img
+                    width={64}
+                    height={64}
+                    //className="mr-3"
+                    src={props.data.imageUrl}
+                    alt="Image"
+                />
+            </div>
+            <div class="flex-grow-1 ms-3">
+                <h5>{props.data.productName}</h5>
+                <small class="text-muted"><i>Released on {props.data.releasedDate}</i></small>
+                <Rating 
+                    rating={props.data.rating} numOfReviews={props.data.numOfReviews}                    
+                />
+                <p>{props.data.description}</p>
+            </div>
         </div>
     );
 }

80e05035df4167c73bc3843053c84c16abc7914b
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-11-16 12:51:14 -0500

    Handling events with states

--- my_react_app/src/Components/Rating.jsx
+++ my_react_app/src/Components/Rating.jsx
@@ -10,28 +10,28 @@ function Rating(props) {
             <h2>Rating: {rating}</h2>
             {
                 (rating >= 1) ?
-                (<IoIosStar />) :
-                (<IoIosStarOutline />)
+                (<IoIosStar onClick={() => setRating(1)} />) :
+                (<IoIosStarOutline onClick={() => setRating(1)} />)
             }
             {
                 (rating >= 2) ?
-                (<IoIosStar />) :
-                (<IoIosStarOutline />)
+                (<IoIosStar onClick={() => setRating(2)} />) :
+                (<IoIosStarOutline onClick={() => setRating(2)} />)
             }
             {
                 (rating >= 3) ?
-                (<IoIosStar />) :
-                (<IoIosStarOutline />)
+                (<IoIosStar onClick={() => setRating(3)} />) :
+                (<IoIosStarOutline onClick={() => setRating(3)} />)
             }
             {
                 (rating >= 4) ?
-                (<IoIosStar />) :
-                (<IoIosStarOutline />)
+                (<IoIosStar onClick={() => setRating(4)} />) :
+                (<IoIosStarOutline onClick={() => setRating(4)} />)
             }
             {
                 (rating >= 5) ?
-                (<IoIosStar />) :
-                (<IoIosStarOutline />)
+                (<IoIosStar onClick={() => setRating(5)} />) :
+                (<IoIosStarOutline onClick={() => setRating(5)} />)
             }
         </div>
     );

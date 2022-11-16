d01a81dfb7ac5e0dda34eededfab236e23638457
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-11-16 11:23:03 -0500

    Displaying a list of products

--- my_react_app/src/Components/Products.jsx
+++ my_react_app/src/Components/Products.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 function Products() {
+
+    const products = ["Boston Dynamics' Atlas", "IIT's iCub", "DLR's Toro" , "Tesla's Optimus"];
+    const listProducts = products.map( product => 
+        <li key={product.toString()}>{product}</li> );
+
     return (
         <div>
             <h1>Products</h1>
+            <ul>{listProducts}</ul>
         </div>
     );
 }

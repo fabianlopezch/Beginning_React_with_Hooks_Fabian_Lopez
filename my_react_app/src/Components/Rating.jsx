3d353161572631c4301294f83c0973fd695b5bad
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-11-16 12:19:22 -0500

    Using conditional rendering to add the IoIosStar and IoIosStarOutline ionicons

--- my_react_app/src/Components/Rating.jsx
+++ my_react_app/src/Components/Rating.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { IoIosStar, IoIosStarOutline } from "react-icons/io";
 
 function Rating(props) {
     return (
         <div>
             <h2>Rating: {props.rating}</h2>
+            {
+                (props.rating >= 1) ?
+                (<IoIosStar />) :
+                (<IoIosStarOutline />)
+            }
+            {
+                (props.rating >= 2) ?
+                (<IoIosStar />) :
+                (<IoIosStarOutline />)
+            }
+            {
+                (props.rating >= 3) ?
+                (<IoIosStar />) :
+                (<IoIosStarOutline />)
+            }
+            {
+                (props.rating >= 4) ?
+                (<IoIosStar />) :
+                (<IoIosStarOutline />)
+            }
+            {
+                (props.rating >= 5) ?
+                (<IoIosStar />) :
+                (<IoIosStarOutline />)
+            }
         </div>
     );
 }

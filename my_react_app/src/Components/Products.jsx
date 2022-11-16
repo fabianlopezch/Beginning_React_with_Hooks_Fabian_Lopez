2e832468e2ca1fab90b93c534d6e8631e65f0d3d
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-11-16 16:07:43 -0500

    Hardcoding an array of product objects

--- my_react_app/src/Components/Products.jsx
+++ my_react_app/src/Components/Products.jsx
@@ -2,14 +2,39 @@ import React from "react";
 
 function Products() {
 
-    const products = ["Boston Dynamics' Atlas", "IIT's iCub", "DLR's Toro" , "Tesla's Optimus"];
-    const listProducts = products.map( product => 
-        <li key={product.toString()}>{product}</li> );
+    // getProduct function simulates data received from a server
+    const getProducts = () => {
+        return [
+            {
+                imageUrl: "https://loremflickr.com/150/150?random=1",
+                productName: "Product 1",
+                releasedDate: "May 31, 2016",
+                description: "Description ... \nDescription ...",
+                rating: 4,
+                numOfReviews: 2
+            },
+            {
+                imageUrl: "https://loremflickr.com/150/150?random=2",
+                productName: "Product 2",
+                releasedDate: "December 15, 2017",
+                description: "Description ... \nDescription ...",
+                rating: 5,
+                numOfReviews: 24
+            },
+            {
+                imageUrl: "https://loremflickr.com/150/150?random=3",
+                productName: "Product 3",
+                releasedDate: "June 5, 2015",
+                description: "Description ... \nDescription ...",
+                rating: 2,
+                numOfReviews: 15
+            }
+        ];
+    };
 
     return (
         <div>
-            <h1>Products</h1>
-            <ul>{listProducts}</ul>
+            
         </div>
     );
 }

a10b08a7c5c78e2a5a0caca63e9ec3b0ef1364a8
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-11-16 11:06:50 -0500

    Embedding JavaScript expressions in JSX

--- my_react_app/src/App.jsx
+++ my_react_app/src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Products from "./Components/Products";
 
+function formatName(user){
+  return user.firstName + ' ' + user.lastName;
+}
+
 function App() {
+
+  const user = {
+    firstName: "Fabian Marcelo",
+    lastName: "Lopez Chamorro"
+  };
+
   return (
     <div>
-      <h1>Hello, Fabian Lopez</h1>
+      <h1>Hello, {formatName(user)}</h1>
       <Products />
     </div>
   );

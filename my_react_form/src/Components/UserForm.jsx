ff42bd7af36efe0f15e0ab2d98c78ae0aa003f97
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-11-16 23:25:08 -0500

    Handling form inputs with useState

--- my_react_form/src/Components/UserForm.jsx
+++ my_react_form/src/Components/UserForm.jsx
@@ -1,31 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function UserForm() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
     return (
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-  
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        {/* 
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-        */}
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+        <div>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" 
+                    onChange={event => setEmail(event.target.value)} />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+                </Form.Group>
+        
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" 
+                    onChange={event => setPassword(event.target.value)}/>
+                </Form.Group>
+                {/* 
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                */}
+                <Button variant="primary" type="submit">
+                Submit
+                </Button>
+            </Form>
+            Email entered: {email}
+            <br />
+            Password entered: {password}
+        </div>
     );
   }
   
